9a93dd95a0655c59934fe29e5bc19e6fda366035
Walter <zikavaclav05@gmail.com>
2022-02-04 15:19:43 +0100

    Responsive

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ app = Flask(__name__)
 @app.route("/", methods = ["GET", "POST"])
 def index():
     print("here")
-    ok_ltrs = request.args.getlist("ok_ltrs")
+    if request.args.getlist("ok_ltrs"):
+        list = request.args.getlist("ok_ltrs")
+        ok_ltrs = str(''.join(str(l) for l in list))
+
+    elif request.args.get("ok_ltrs_mobile"):
+        ok_ltrs = request.args.getlist("ok_ltrs")
+
+    else:
+        ok_ltrs = ""
+
     need_ltr = request.args.get("need_ltr")
     eng = request.args.get("eng")
     capitals = request.args.get("capitals")
@@ -15,8 +24,6 @@ def index():
     length = request.args.get("length")
 
     if ok_ltrs and need_ltr:
-        ok_ltrs = str(''.join(str(l) for l in ok_ltrs))
-
         h = HomeworkWriter(ok_ltrs, str(need_ltr), bool(eng), bool(capitals), str(seperator), min_length_word, length) 
         return render_template("index.html", homework=h.text)
 
