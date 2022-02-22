68e13a296dc936152d759709f1de795e8e0c5022
Walter <zikavaclav05@gmail.com>
2022-02-22 22:56:07 +0100

    local_storage

--- static/js/main.js
+++ static/js/main.js
@@ -2,27 +2,29 @@ function SetToLS(name, data) {
     localStorage.setItem(name, data);
 }
 
-function FillInputsFromLs() {
-    // Setting fullscreen of pudorys by local storage
-    if (localStorage.getItem("showFullscreen") !== null) {
-        if (localStorage.getItem("showFullscreen") === "true") {
-            document.getElementById("fullscreenEnabler").click();
+function FillInputsFromLS() {
+    for (var i = 0; i < localStorage.length; i++){
+        let itemId = localStorage.key(i)
+        let itemValue = localStorage.getItem(localStorage.key(i));
+
+        if (itemId == "seperator" || itemId == "mobile_keyboard" || itemId == "min_length_word" || itemId == "length") {
+            document.getElementById(itemId).value = itemValue;
+        } else {
+            // Checkbox
+            if (JSON.parse(itemValue)) {
+                document.getElementById(itemId).checked = true;
+            } else {
+                document.getElementById(itemId).checked = false;
+            }
         }
     }
+}
 
-    // Setting reservation visibility by local storage
-    if (localStorage.getItem("showReservations") !== null) {
-        if (localStorage.getItem("showReservations") === "false") {
-            document.getElementById("checkResEnabler").click();
-        }
-    }
-
-    // Setting skipped imgs visibility by local storage
-    if (localStorage.getItem("eng") !== null) {
-        if (localStorage.getItem("eng") === "true") {
-            document.getElementById("eng").click();
-        }
-    }
-
-
+function copyToClipboard(text) {
+    const el = document.createElement('textarea');
+    el.value = text;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
 }
